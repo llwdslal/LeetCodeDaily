4b315b4e1bfd14fc82f5c75fd1b841912eedaafd
rocklee <466186888@qq.com>
2021-02-23 16:25:35 +0800

    init

---
+++ src/GrumpyBookstoreOwner_1052_mid.java
@@ -0,0 +1,56 @@
+/***
+ * 今天，书店老板有一家店打算试营业 customers.length 分钟。
+ *
+ * 每分钟都有一些顾客（customers[i]）会进入书店，所有这些顾客都会在那一分钟结束后离开。
+ *
+ * 在某些时候，书店老板会生气。 如果书店老板在第 i 分钟生气，那么 grumpy[i] = 1，否则 grumpy[i] = 0。
+ *
+ * 当书店老板生气时，那一分钟的顾客就会不满意，不生气则他们是满意的。
+ *
+ * 书店老板知道一个秘密技巧，能抑制自己的情绪，可以让自己连续 X 分钟不生气，但却只能使用一次。
+ *
+ * 请你返回这一天营业下来，最多有多少客户能够感到满意的数量。
+ *
+ */
+
+public class GrumpyBookstoreOwner_1052_mid {
+
+    public static void main(String[] args) {
+        int[] customers = {1,0,1,2,1,1,7,5};
+        int[] grumpy =    {0,1,0,1,0,1,0,1};
+        int X = 3;
+        System.out.println(maxSatisfied(customers,grumpy,X));
+    }
+
+    public static int maxSatisfied(int[] customers, int[] grumpy, int X) {
+        int total = 0;
+        for(int i = 0; i < customers.length; i++){
+            if(grumpy[i] == 0){
+                total += customers[i];
+            }
+        }
+        // 此时 total 的值为 老板未发动秘技留下的客户数
+
+        // 发动秘技： 固定长度X的窗口 ，窗口从左向右移动，
+        // 每次移动计算出 因秘技所增加的客户数 ，即 窗口中 grumpy 数组中（grumpy[index]）等于 1 时索引位置对应 customers 数组中客户数(customers[index])的和
+        // 每次移动因秘技所增加的客户数的最大值就是 发动秘技所能留下客户数的最大值
+        // 最多有多少客户能够感到满意的数量 = 老板未发动秘技留下的客户数 + 发动秘技所能留下客户数的最大值
+
+        // 初始化窗口
+        int increase = 0;// 因秘技所增加的客户数
+        for(int i = 0; i < X; i++){
+            // 不生气是 0 ， 生气是 1 => customers[i] * grumpy[i] 索引为 i 时 因秘技所增加的客户数
+            increase += customers[i] * grumpy[i] ;
+        }
+
+        int maxIncrease = increase;
+        //向右移动窗口
+        for(int i = 0; i < customers.length - X; i ++){
+            //移动后窗口中因秘技所增加的客户数为 减去现在窗口左边界因秘技所增加的客户数 加上右边界下一个因秘技所增加的客户数
+            increase = increase - customers[i] * grumpy[i] + customers[i+X] * grumpy[i+X];
+            maxIncrease = Math.max(maxIncrease,increase);
+        }
+        return total + maxIncrease;
+
+    }
+}
---
+++ src/ToeplitzMatrix_766_easy.java
@@ -0,0 +1,39 @@
+/***
+ * 给你一个 m x n 的矩阵 matrix 。如果这个矩阵是托普利茨矩阵，返回 true ；否则，返回 false 。
+ *
+ * 如果矩阵上每一条由左上到右下的对角线上的元素都相同，那么这个矩阵是 托普利茨矩阵 。
+
+ * 输入：matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+ * 输出：true
+ *
+ * [1,2,3,4]
+ * [5,1,2,3]
+ * [9,5,1,2]
+ *
+ * 解释：
+ * 在上述矩阵中, 其对角线为:
+ * "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]"。
+ * 各条对角线上的所有元素均相同, 因此答案是 True 。
+ *
+ */
+
+public class ToeplitzMatrix_766_easy {
+    public static void main(String[] args) {
+        int[][] matrix =  {{1,2,3,4},{5,1,2,3},{9,5,1,2}};
+        System.out.println(isToeplitzMatrix(matrix));;
+    }
+
+    public static boolean isToeplitzMatrix(int[][] matrix) {
+        for(int i = 0; i < matrix.length - 1; i++){//最后一行不需要比较
+            int[] row = matrix[i];
+            int[] nextRow = matrix[i+1];
+            for (int j = 0; j < row.length - 1;j++){//每行的最后一个元素不需要比较
+                //如果当前行索引为 j 的元素 与 下一行索引为 j+1 的元素（右下的元素）是否相等
+                if (row[j] != nextRow[j+1]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
